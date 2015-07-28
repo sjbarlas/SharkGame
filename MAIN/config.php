71b749753bd1b681f70bf7174905b1284278331c
Saira <c12809080@mydit.ie>
2015-07-28 21:47:52 +0100

    aha

--- MAIN/config.php
+++ MAIN/config.php
@@ -1,7 +1,16 @@
 <?php
-    $host="localhost"; // Host name 
-    $username="root"; // Mysql username 
-    $password="root"; // Mysql password 
-    $db_name="login"; // Database name 
-    $tbl_name="members"; // Table name
+    // These variables define the connection information for your MySQL database 
+    $username = "root"; 
+    $password = ""; 
+    $host = "localhost"; 
+    $dbname = "users"; 
+     
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'); 
+    try { $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options); } 
+    catch(PDOException $ex){ die("Failed to connect to the database: " . $ex->getMessage());} 
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
+    header('Content-Type: text/html; charset=utf-8'); 
+    session_start(); 
 ?>
+
