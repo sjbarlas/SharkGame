468c6bd5c0e3ae4ba7a9a06dc83f95fdb5554adf
Saira <c12809080@mydit.ie>
2015-07-29 16:19:42 +0100

    wow

--- MAIN/main_login.php
+++ MAIN/main_login.php
@@ -1,23 +1,27 @@
 <?php
-  session_start();
+session_start();
 
-  if(isset($_SESSION['username'])){
-    header("location:index.php");
-  }
+if(isset($_SESSION['username'])){
+  header("location:index.php");
+}
 ?><!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Login</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
-    <link href="css/main.css" rel="stylesheet" media="screen">
-  </head>
-  
-  <body>
-    <div class="container">
+<head>
+  <meta charset="utf-8">
+  <title>Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+  <link href="signin.css" rel="stylesheet" media="screen">
+  <link href="footer.css" rel="stylesheet" media="screen">
+</head>
 
+<body>
+  <br>
+  <br>
+  <br>
+  <div class="text-center">
+    <div class="container">
       <form class="form-signin" name="form1" method="post" action="checklogin.php">
         <h2 class="form-signin-heading">Please sign in</h2>
         <input name="myusername" id="myusername" type="text" class="form-control" placeholder="Username" autofocus>
@@ -26,20 +30,27 @@
         <label class="checkbox">
           <input type="checkbox" value="remember-me"> Remember me
         </label>
-        -->
-        <button name="Submit" id="submit" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      -->
+      <button name="Submit" id="submit" class="btn btn-info btn-primary btn-block" type="submit"><strong>SIGN IN</strong></button>
 
-        <div id="message"></div>
-      </form>
+      <div id="message"></div>
+    </form>
 
-    </div> <!-- /container -->
+  </div> <!-- container -->
+</div> <!-- centre text -->
+<footer class="footer"><a href="about.html">
+  <div class="container"><a href="about.html">
+    <br>
+    <p class="text-center">SHARK PROJECT</p>
+  </div></a>
+</footer></a>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="//code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script type="text/javascript" src="js/bootstrap.js"></script>
-    <!-- The AJAX login script -->
-    <script src="js/login.js"></script>
-    
-  </body>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="//code.jquery.com/jquery.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<!-- The AJAX login script -->
+<script src="js/login.js"></script>
+
+</body>
 </html>
