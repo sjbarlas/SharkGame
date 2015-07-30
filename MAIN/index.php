ba6d48a00fb76139193461b5e8ce66a64fd7c84b
Saira <c12809080@mydit.ie>
2015-07-30 09:10:11 +0100

    agh

--- MAIN/index.php
+++ MAIN/index.php
@@ -1,23 +1,23 @@
 <?php
-  session_start();
+session_start();
 
-  if(!isset($_SESSION['username'])){
-    header("location:main_login.php");
-  }
+if(!isset($_SESSION['username'])){
+  header("location:main_login.php");
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Sign in</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
-    <link href="home.css" rel="stylesheet" media="screen">
-  </head>
-  <body>
-    <div class="text-center">
+<head>
+  <meta charset="utf-8">
+  <title>Sign in</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap -->
+  <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+  <link href="home.css" rel="stylesheet" media="screen">
+</head>
+<body>
+  <div class="text-center">
     <div class="container">
       <div class="form-signin">
         <div class="alert alert-success">You have been <strong>successfully logged in</strong>.</div>
@@ -25,5 +25,5 @@
       </div>
     </div> <!-- /container -->
   </div>
-  </body>
+</body>
 </html>
