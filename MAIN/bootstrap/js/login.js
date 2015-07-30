4624867c6d16907fc88a250d67284c40f818276c
Saira <c12809080@mydit.ie>
2015-07-30 11:08:42 +0100

    user

--- MAIN/bootstrap/js/login.js
+++ MAIN/bootstrap/js/login.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-    
+
+  // when the user clicks on the login button    
   $("#submit").click(function(){
 
+    // get each input value in a veriable
     var username = $("#myusername").val();
     var password = $("#mypassword").val();
-    
+
+    // check if the username and/or the password input are empty
     if((username == "") || (password == "")) {
       $("#message").html("<div class=\"alert alert-danger alert-dismissable\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">&times;</button>Please enter a username and a password</div>");
     }
@@ -14,15 +17,15 @@ $(document).ready(function(){
         url: "checklogin.php",
         data: "myusername="+username+"&mypassword="+password,
         success: function(html){    
-          if(html=='true') {
+          if(html=='true') { // if the return value = 'true' then redirect to 'index.php'
             window.location="index.php";
           }
-          else {
+          else { // if the return value != 'true' then add the error message to the div.#message
             $("#message").html(html);
           }
         },
         beforeSend:function()
-        {
+        { // loading gif 
           $("#message").html("<p class='text-center'><img src='images/ajax-loader.gif'></p>")
         }
       });
