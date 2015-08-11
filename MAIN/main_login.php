a46fbc1ef4cabf96f410118f50c1af27b155ba7f
Saira <c12809080@mydit.ie>
2015-08-11 12:07:07 +0100

    new

--- MAIN/main_login.php
+++ MAIN/main_login.php
@@ -14,7 +14,7 @@ if(isset($_SESSION['username'])){
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Login</title>
+  <title>Sign In | SHARK PROJECT</title>
   <!-- Bootstrap -->
   <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
   <link href="sharkcss/main_loginSignin.css" rel="stylesheet">
