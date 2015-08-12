c8365faf3db3287924acd57eceb91e828b76d07b
Saira <c12809080@mydit.ie>
2015-08-12 12:04:12 +0100

    wowzers

--- MAIN/index.php
+++ MAIN/index.php
@@ -20,13 +20,33 @@ if(!isset($_SESSION['username'])){
   <link href="sharkcss/index.css" rel="stylesheet">
 </head>
 <body>
-  <div class="text-center">
-    <div class="container">
-      <div class="form-signin">
-        <div class="alert alert-success">You have been <strong>successfully signed in</strong>.</div>
-        <a href="signout.php" class="btn btn-info btn-sm">SIGN OUT</a>
-      </div>
-    </div> <!-- container -->
-  </div> <!-- text centre -->
+ <header class="top" role="header">
+  <div class="container">
+    <a href="about.html" class="navbar-brand pull-left">
+      SHARK PROJECT
+    </a>
+    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <span class="glyphicon glyphicon-align-justify"></span>
+    </button>
+    <nav class="navbar-collapse collapse" role="navigation">
+      <ul class="navbar-nav nav">
+        <li><a href="game.html">Game</a></li>
+        <li><a href="sguide.html">Shark Guide</a></li>
+        <li><a href="leaderboard.html">Leaderboard</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="main_signin.php">Sign In</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+<div class="text-center">
+  <div class="container">
+    <div class="form-signin">
+      <div class="alert alert-success">You have been <strong>successfully signed in</strong>.</div>
+      <a href="signout.php" class="btn btn-info btn-sm">SIGN OUT</a>
+    </div>
+  </div> <!-- container -->
+</div> <!-- text centre -->
 </body>
 </html>
