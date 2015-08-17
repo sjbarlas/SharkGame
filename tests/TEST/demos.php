5c407b5bba4f828a39e2a9188d94ab2dff187ea8
Saira <c12809080@mydit.ie>
2015-08-17 11:59:45 +0100

    indent

--- tests/TEST/demos.php
+++ tests/TEST/demos.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>mar gai</title>
-<link href="http://edge1y.tapmodo.com/deepliq/global.css" rel="stylesheet" type="text/css" />
-<link rel="stylesheet" href="http://jcrop-cdn.tapmodo.com/v0.9.12/css/jquery.Jcrop.min.css" type="text/css" />
-<link href="http://edge1u.tapmodo.com/deepliq/jcrop_demos.css" rel="stylesheet" type="text/css" />
+	<title>mar gai</title>
+	<link href="http://edge1y.tapmodo.com/deepliq/global.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="http://jcrop-cdn.tapmodo.com/v0.9.12/css/jquery.Jcrop.min.css" type="text/css" />
+	<link href="http://edge1u.tapmodo.com/deepliq/jcrop_demos.css" rel="stylesheet" type="text/css" />
 
-<script src="http://edge1u.tapmodo.com/global/js/jquery.min.js"></script>
-<script src="http://jcrop-cdn.tapmodo.com/v0.9.12/js/jquery.Jcrop.min.js"></script>
-<script src="http://edge1v.tapmodo.com/deepliq/jcrop_demos.js"></script>
+	<script src="http://edge1u.tapmodo.com/global/js/jquery.min.js"></script>
+	<script src="http://jcrop-cdn.tapmodo.com/v0.9.12/js/jquery.Jcrop.min.js"></script>
+	<script src="http://edge1v.tapmodo.com/deepliq/jcrop_demos.js"></script>
 </head>
 <body>
 
-<div id="pholder">
-	<div id="pbox">
-		<div class="headbar">
-			<div id="headlinks">
+	<div id="pholder">
+		<div id="pbox">
+			<div class="headbar">
+				<div id="headlinks">
+				</div>
 			</div>
-		</div>
 
 			<div id ="sidemenu">
 				<div class="menu">
@@ -35,15 +35,15 @@
 					<big><b>Hello World!</b></big>
 				</div>
 			</div>
+		</div>
 	</div>
-</div>
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-5176876-1");
-pageTracker._trackPageview();
-</script>
+	<script type="text/javascript">
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+	var pageTracker = _gat._getTracker("UA-5176876-1");
+	pageTracker._trackPageview();
+	</script>
 </body>
 </html>
