e4e5ba11df240c0f6abb023b0ebc96cbd6d4612a
Saira <c12809080@mydit.ie>
2015-08-19 12:41:24 +0100

    tag

--- MAIN/indexing.php
+++ MAIN/indexing.php
@@ -1,8 +1,14 @@
 <html>
 <head>
 	<title>Tagging System</title>
+	<link rel='stylesheet' href='style.css' />
 </head>
 <body>
 
+<img src='images/1.jpg' />
+
+<script src='jquery.js'></script>
+<script src='index.js'></script>
+</script>
 </body>
 </html>
