e7af47456d9d7e6bc438e369ea3aa7f03ff1d5fe
Saira <c12809080@mydit.ie>
2015-08-28 10:59:17 +0100

    moreeeeeeee

--- MAIN/taggingSystem.php
+++ MAIN/taggingSystem.php
@@ -1,52 +1,36 @@
-<!-- IF THE USER IS SUCCESFUL IN SIGNING IN -->
-
-<?php
-session_start();
-
-if(!isset($_SESSION['username'])){
-  header("location:main_signin.php");
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Sign in</title>
-  <!-- Bootstrap -->
-  <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-  <link href="sharkcss/index.css" rel="stylesheet">
+	<title>Tagging System</title>
+	<link rel='stylesheet' href='style.css' />
 </head>
 <body>
- <header class="top" role="header">
-  <div class="container">
-    <a href="about.html" class="navbar-brand pull-left">
-      SHARK PROJECT
-    </a>
-    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-      <span class="glyphicon glyphicon-align-justify"></span>
-    </button>
-    <nav class="navbar-collapse collapse" role="navigation">
-      <ul class="navbar-nav nav">
-        <li><a href="game.html">Game</a></li>
-        <li><a href="sguide.html">Shark Guide</a></li>
-        <li><a href="leaderboard.html">Leaderboard</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="main_signin.php">Sign In</a></li>
-        <li><a href="signout.php" class="btn btn-info btn-sm">SIGN OUT</a></li>
-      </ul>
-    </nav>
-  </div>
-</header>
-<!-- pop up message -->
-<div class="text-center">
-  <div class="container">
-    <div class="form-signin">
-      <div class="alert alert-success">You have been <strong>successfully signed in</strong>.</div>
-    </div>
-  </div> <!-- container -->
-</div> <!-- text centre -->
+
+<!--<?php
+
+	//include 'connect.php';
+
+	/*$query = mysql_query("SELECT 'name', 'top', 'left' FROM 'temptag'");
+	while($run = mysql_fetch_array($query))
+	{
+		$name = $run['name'];
+		$top = $run['top'];
+		$left = $run['left'];
+	}
+
+} */
+?> -->
+
+<!--<div id='tagged' style='top: <?php //echo $top; ?>; left: <?php //echo $left; ?>'><?php //echo $name; ?></div> -->
+
+<!--<?php?>-->
+
+<img src='images/1.jpg' id='image' />
+
+<!--<div>-->
+	<input type='text' id='name' style='display:none; position:absolute' />
+<!--</div>-->
+
+<script src='jquery.js'></script> 
+<script src='indexing.js'></script> <!-- this is added for the indexing -->
 </body>
 </html>
