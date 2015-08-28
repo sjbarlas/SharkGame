04691753fbabda0bc9e445122b2fc0a6aa11c54d
sjbarlas <c12809080@mydit.ie>
2015-08-28 16:03:06 +0100

    this

--- MAIN/testing-tagging/taggingSystemConnect.php
+++ MAIN/testing-tagging/taggingSystemConnect.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 	mysql_connect('localhost', 'root', 'sharks');
-=======
-	mysql_connect('localhost', 'root', 'sairabarlas');
->>>>>>> c52d03f6817c781a2512c59b7967d42ef57dba43
 
 	mysql_select_db('sharks');
 
