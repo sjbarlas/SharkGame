bfe404aaf6765f2be666c71050ffb9d954d904eb
sjbarlas <c12809080@mydit.ie>
2015-08-28 15:33:39 +0100

    pw

--- MAIN/config.php
+++ MAIN/config.php
@@ -5,11 +5,7 @@
 
 	$host="localhost"; // host name 
 	$username="root"; // mysql username 
-<<<<<<< HEAD
-	$password="sharks"; // mysql password 
-=======
-	$password=""; // mysql password 
->>>>>>> c52d03f6817c781a2512c59b7967d42ef57dba43
+	$password="sharks"; // mysql password
 	$db_name="sharks"; // database name 
 	$tbl_name="user"; // table name
 	?>
